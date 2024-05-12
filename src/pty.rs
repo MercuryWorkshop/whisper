84ee5cacbd1ceaea39bcba023b7bc87eaf72c2b1
Toshit Chawda <r58playz@gmail.com>
2024-05-12 13:07:40 -0700

    force a flush every write

--- src/pty.rs
+++ src/pty.rs
@@ -1,10 +1,6 @@
 use std::{io, os::fd::AsFd, path::PathBuf};
 
-use bytes::Bytes;
-use futures_util::{
-    stream::{SplitSink, SplitStream},
-    SinkExt, StreamExt,
-};
+use futures_util::{SinkExt, StreamExt};
 use tokio::fs::File;
 use tokio_util::codec::{Framed, LengthDelimitedCodec};
 use wisp_mux::{
@@ -13,19 +9,27 @@ use wisp_mux::{
 };
 
 pub async fn open_pty(file: &PathBuf) -> Result<(PtyRead, PtyWrite), io::Error> {
-    let pty = File::options().read(true).write(true).open(file).await?;
-    let mut termios = nix::sys::termios::tcgetattr(pty.as_fd())?.clone();
+    let rx = File::options().read(true).open(file).await?;
+    let mut termios = nix::sys::termios::tcgetattr(rx.as_fd())?.clone();
     nix::sys::termios::cfmakeraw(&mut termios);
-    nix::sys::termios::tcsetattr(pty.as_fd(), nix::sys::termios::SetArg::TCSANOW, &termios)?;
-    let pty = LengthDelimitedCodec::builder()
+    nix::sys::termios::tcsetattr(rx.as_fd(), nix::sys::termios::SetArg::TCSANOW, &termios)?;
+    let rx = LengthDelimitedCodec::builder()
         .little_endian()
         .max_frame_length(usize::MAX)
-        .new_framed(pty);
-    let (tx, rx) = pty.split();
+        .new_framed(rx);
+
+    let tx = File::options().write(true).open(file).await?;
+    let mut termios = nix::sys::termios::tcgetattr(tx.as_fd())?.clone();
+    nix::sys::termios::cfmakeraw(&mut termios);
+    nix::sys::termios::tcsetattr(tx.as_fd(), nix::sys::termios::SetArg::TCSANOW, &termios)?;
+    let tx = LengthDelimitedCodec::builder()
+        .little_endian()
+        .max_frame_length(usize::MAX)
+        .new_framed(tx);
     Ok((PtyRead(rx), PtyWrite(tx)))
 }
 
-pub struct PtyRead(SplitStream<Framed<File, LengthDelimitedCodec>>);
+pub struct PtyRead(Framed<File, LengthDelimitedCodec>);
 
 impl WebSocketRead for PtyRead {
     async fn wisp_read_frame(
@@ -43,7 +47,7 @@ impl WebSocketRead for PtyRead {
     }
 }
 
-pub struct PtyWrite(SplitSink<Framed<File, LengthDelimitedCodec>, Bytes>);
+pub struct PtyWrite(Framed<File, LengthDelimitedCodec>);
 
 impl WebSocketWrite for PtyWrite {
     async fn wisp_write_frame(&mut self, frame: Frame) -> Result<(), wisp_mux::WispError> {
@@ -60,6 +64,12 @@ impl WebSocketWrite for PtyWrite {
                 .await
                 .map_err(|x| WispError::WsImplError(Box::new(x))),
             _ => Err(WispError::WsImplNotSupported),
-        }
+        }?;
+        self.0
+            .get_mut()
+            .sync_data()
+            .await
+            .map_err(|x| WispError::WsImplError(Box::new(x)))?;
+        Ok(())
     }
 }
